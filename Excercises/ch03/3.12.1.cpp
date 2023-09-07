3855e9e97fe3573afff9e64d8ef50a439ea8948c
998372 <shangwen193@gmail.com>
2023-09-07 09:23:21 -0400

    Fixed file type

---
+++ Excercises/ch03/3.12.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void new_line()
+{
+    cout << endl;
+}
+
+int main()
+{
+    cout << "First Line." << endl;
+    new_line();
+    cout << "Second Line." << endl;
+    return 0;
+}
