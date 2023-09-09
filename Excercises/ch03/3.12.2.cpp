7d409107eaac76ed6a4b09a731bb8ec86d5c2d25
998372 <shangwen193@gmail.com>
2023-09-09 00:26:30 -0400

    Created and combined original excercise code

---
+++ Excercises/ch03/3.12.2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void print_twice(char phil) {
+    cout << phil << phil << endl;
+}
+
+int main() {
+    print_twice('a');
+    return 0;
+
+    char argument = 'b';
+    print_twice(argument);
+    return 0;
+}
