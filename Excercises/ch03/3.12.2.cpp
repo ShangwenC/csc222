d19b8d604d884df4fa1f05249f7f6338bb04b855
998372 <shangwen193@gmail.com>
2023-09-09 00:33:28 -0400

    Tried int '6'

--- Excercises/ch03/3.12.2.cpp
+++ Excercises/ch03/3.12.2.cpp
@@ -5,12 +5,16 @@ using namespace std;
 void print_twice(char phil) {
     cout << phil << phil << endl;
 }
-
-int main() {
+  
+  int main() {
     print_twice('a');
     return 0;
 
     char argument = 'b';
     print_twice(argument);
     return 0;
+
+    print_twice('6');
+    return 0;
 }
+
