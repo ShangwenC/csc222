8fa677a8fc7c540d38e7744ef755165579d94088
998372 <shangwen193@gmail.com>
2023-09-09 00:18:13 -0400

    Added note about excercise result

--- Excercises/ch03/3.12.1.cpp
+++ Excercises/ch03/3.12.1.cpp
@@ -15,3 +15,4 @@ void new_line()
     cout << endl;
 }
 
+//Recieved error message "error: use of undeclared identifier 'new_line'". This is because the function must be declared before it can be used.
