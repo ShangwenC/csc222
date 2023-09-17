49d71f8314a145a80ad413b13edbe0df02d532f5
998372 <shangwen193@gmail.com>
2023-09-17 09:54:56 -0400

    Finished excercise

--- Excercises/ch05/5.14.1.cpp
+++ Excercises/ch05/5.14.1.cpp
@@ -6,8 +6,8 @@ double distance(double x1, double y1, double x2, double y2) {
     double dx = x2 - x1;
     double dy = y2 - y1;
     double dsquared = dx * dx + dy * dy;
-    cout << "dsquared is " << dsquared << endl;
-    return 0.0;
+    double result = sqrt(dsquared);
+    return result;
 }
 
 int main()
