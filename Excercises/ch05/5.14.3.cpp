76859403a450e24a4c1967f4173330e97e4345f3
998372 <shangwen193@gmail.com>
2023-09-17 10:21:04 -0400

    fourth function

--- Excercises/ch05/5.14.3.cpp
+++ Excercises/ch05/5.14.3.cpp
@@ -20,10 +20,18 @@ void overloaded_function(string emotion) {
     }
 }
 
+void overloaded_function(int n, string n2) {
+    if ((n == 5) && (n2 == "5")) {
+        cout << "your numbers are both 5";
+    }
+}
+
+
 int main()
 {
     overloaded_function(12, 4);
     overloaded_function(1999);
     overloaded_function("happy");
+    overloaded_function(5,"5");
     return 0;
 }
