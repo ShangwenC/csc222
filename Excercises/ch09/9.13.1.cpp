830bd4be786da8012ef630114ee8bd2307aa072d
998372 <shangwen193@gmail.com>
2023-10-01 11:54:14 -0400

    Got errors when compiling

---
+++ Excercises/ch09/9.13.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void easy_function (const, const)
+{
+    if ((const + const) = 5) {
+        cout << "yay!";
+    }
+}
+
+int main ()
+{
+    easy_function(2, 3);
+    return 0;
+}
