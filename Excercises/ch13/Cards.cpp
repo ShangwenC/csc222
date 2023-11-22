226ba9c2e10793f22f5aec6c5f11098a57739fae
998372 <shangwen193@gmail.com>
2023-11-22 03:25:54 -0500

    member functions

--- Excercises/ch13/Cards.cpp
+++ Excercises/ch13/Cards.cpp
@@ -4,3 +4,35 @@
 #include <string>
 using namespace std;
 
+string Card::to_string() const
+{
+    vector<string> suit_strings = {"None", "Clubs", "Diamonds",
+                                   "Hearts", "Spades"};
+    vector<string> rank_strings = {"Joker", "2", "3", "4", "5", "6", "7",
+                                   "8", "9", "10", "Jack", "Queen", "King",
+                                   "Ace"};
+
+    if (rank == 0) return rank_strings[rank];
+    return rank_strings[rank] + " of " + suit_strings[suit];
+}
+
+bool Card::equals(const Card& c2) const
+{
+    return (rank == c2.rank && suit == c2.suit);
+}
+
+bool Card::is_greater(const Card& c2) const
+{
+    // first check the suits
+    if (suit > c2.suit) return true;
+    if (suit < c2.suit) return false;
+
+    // if suits are equal, check ranks
+    if (rank > c2.rank) return true;
+    if (rank < c2.rank) return false;
+    // this last statement can be omitted without changing the
+    // behavior of the function, but making it arguably less readable
+
+    // if ranks are equal too, 1st card is not greater than the 2nd
+    return false;
+}
