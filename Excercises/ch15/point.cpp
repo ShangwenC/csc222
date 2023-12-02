96751cef79c23adb21965e4025bce2132ba315e5
998372 <shangwen193@gmail.com>
2023-12-02 01:54:34 -0500

    reflect and distance functions

--- Excercises/ch15/point.cpp
+++ Excercises/ch15/point.cpp
@@ -12,6 +12,20 @@ void print_point(Point p)
     cout << "(" << p.x << ", " << p.y << ")" << endl;
 }
 
+double distance(Point p1, Point p2)
+{
+    double dx = p2.x - p1.x;
+    double dy = p2.y - p1.y;
+    return sqrt(dx*dx + dy*dy);
+}
+
+void reflect(Point& p)
+{
+    double temp = p.x;
+    p.x = p.y;
+    p.y = temp;
+}
+
 int main () {
     Point blank;
     blank.x = 3.0;
@@ -22,5 +36,7 @@ int main () {
     Point p1 = {3.0, 4.0};
     Point p2 = p1;
     cout << "(" << p2.x << ", " << p2.y << ")" << endl;
-    
+    print_point(blank);
+    reflect(blank);
+    print_point(blank);
 }
