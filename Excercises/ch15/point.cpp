541244aa972d1fc1dddd9475926e5008b9f53520
998372 <shangwen193@gmail.com>
2023-12-02 00:01:17 -0500

    added print point function

--- Excercises/ch15/point.cpp
+++ Excercises/ch15/point.cpp
@@ -7,6 +7,11 @@ struct Point {
     double x, y;
 };
 
+void print_point(Point p)
+{
+    cout << "(" << p.x << ", " << p.y << ")" << endl;
+}
+
 int main () {
     Point blank;
     blank.x = 3.0;
@@ -14,4 +19,8 @@ int main () {
     int x = blank.x;
     cout << "(" << blank.x << ", " << blank.y << ")" << endl;
     double distance = blank.x * blank.x + blank.y * blank.y;
+    Point p1 = {3.0, 4.0};
+    Point p2 = p1;
+    cout << "(" << p2.x << ", " << p2.y << ")" << endl;
+    
 }
