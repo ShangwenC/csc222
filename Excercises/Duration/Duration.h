29c5d547ba26fc50365db70cb5a3d7c592e996c8
Cheng <shangwen193@gmail.com>
2023-12-21 00:44:37 -0500

    header

--- Excercises/Duration/Duration.h
+++ Excercises/Duration/Duration.h
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+Class Duration {
+public:
+  Duration();
+  int seconds();
+  int minutes();
+  int hours();
+  int days();
+  Duration operator + (const Duration& d);
+  Duration operator - (const Duration& d);
+  string to_sring();
+}
